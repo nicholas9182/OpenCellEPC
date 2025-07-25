77160a63fab172985ee9001d324ca6529bd9dc3b
Nicholas Siemons <nicholas9182@gmail.com>
2025-07-25 16:23:56 -0700

    Initial commit: OpenCellEPC project

---
+++ setup.py
@@ -0,0 +1,41 @@
+from distutils.core import setup
+from setuptools import find_packages
+import pathlib
+import re
+
+root = pathlib.Path(__file__).parent
+init = root / "OpenCellEPC" / "__init__.py"
+version = re.search(r'__version__\s*=\s*"([^"]+)"', init.read_text()).group(1)
+
+setup(
+    name='OpenCellProductionCost',
+    version=version, 
+    description='Modelling Production Cost of Stationary Storage Sites',
+    author='Nicholas Siemons',
+    author_email='nsiemons@stanford.edu',
+    url="https://github.com/nicholas9182/OpenCellEPC/",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "numpy",
+        "datetime",
+        "shapely",
+        "plotly",
+        "dash",
+        "dash_bootstrap_components",
+        "flask_caching",
+        "nbformat"
+    ],
+    scripts=[],
+    classifiers=[ 
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+    entry_points={
+        'console_scripts': [
+            'launch_dash_app=dash_app.app:run',
+        ],
+    }
+)
